72fbb25382b91c9bcfcde3a20bbef78a373ce412
hunputer <wlgns3855@gmail.com>
2023-05-19 16:45:47 +0900

    commit

--- src/main/java/com/example/batch/BatchApplication.java
+++ src/main/java/com/example/batch/BatchApplication.java
@@ -3,7 +3,9 @@ package com.example.batch;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 @EnableBatchProcessing
 public class BatchApplication {
